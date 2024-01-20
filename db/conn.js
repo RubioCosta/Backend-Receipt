6b853f1dba963132694331887ef6baf2b65f42dc
Rubio Costa <rubioc02@Gmail.com>
2024-01-20 14:01:35 -0300

    conn

--- db/conn.js
+++ db/conn.js
@@ -1,7 +1,15 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('receipt', 'root', '', {
-    host: 'localhost',
+require('dotenv').config();
+
+// Variables .ENV
+const password = process.env.MYSQLPASSWORD;
+const host = process.env.MYSQLHOST;
+const mysqlPort = process.env.MYSQLPORT;
+
+const sequelize = new Sequelize('receipt', "root", password, {
+    host: host,
+    port: mysqlPort,
     dialect: 'mysql'
 });
 
